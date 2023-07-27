a2224b8a095c916f1c4dd59dbefc8c0571e6d4b4
Duy Nguyen <dtn5ah@virginia.edu>
2023-07-27 17:22:04 -0400

    Jenn Style Review

--- jdaviz/core/launcher.py
+++ jdaviz/core/launcher.py
@@ -171,8 +171,8 @@ class Launcher(v.VuetifyTemplate):
 def show_launcher(configs=ALL_JDAVIZ_CONFIGS, height="100%"):
     # Color defined manually due to the custom theme not being defined yet (in app.vue)
     height = f"{height}px" if isinstance(height, int) else height
-    main = v.Sheet(class_="mx-0",
-                   color="#153A4B",
+    main = v.Sheet(class_="mx-25",
+                   color="#00212C",
                    height=height,
                    _metadata={'mount_id': 'content'})
     main.children = [Launcher(main, configs, height)]
